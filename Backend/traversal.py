b0ee8d52389cde382bd06430f56b3a2d364b02ef
mglittleton <michaelglittleton@gmail.com>
2019-09-05 17:13:02 -0400

    Starting to change to a single user traversal

--- Backend/traversal.py
+++ Backend/traversal.py
@@ -8,29 +8,14 @@ traversalPath = []
 Depth first traversal to where a path ends at dead end or all explored paths,
 then breadth first search to find the closet offshoot that has an unexplored path.
 '''
-api_keys = {
-  'A': "Token 508711f53445fa67d8bdc1c97da256eacaef2e5e",
-  'B': "Token dccec1ad173d2abaf88b542a02095f8d93ea97df",
-  'C': "Token 8271c9035b3a113a16111392722a7bb4d9278a2c",
-  'D': "Token 64936db353e36faa7ec880bb81331706cd4216a7"
-}
-timers = {
-  'A': {'time': 0, 'purpose': 'item lookup'},
-  'B': {'time': 0, 'purpose': 'item lookup'},
-  'C': {'time': 0, 'purpose': 'item lookup'},
-  'D': {'time': 0, 'purpose': 'item lookup'}
-}
-room_info = {}
-room_info['A'] = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers={'Authorization': api_keys['A']})
-room_info['B'] = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers={'Authorization': api_keys['B']})
-room_info['C'] = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers={'Authorization': api_keys['C']})
-room_info['D'] = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers={'Authorization': api_keys['D']})
-item_to_get = {
-  'A': '',
-  'B': '',
-  'C': '',
-  'D': ''
-}
+api_key = "Token 508711f53445fa67d8bdc1c97da256eacaef2e5e"
+header_info = {'Authorization': api_key}
+  # "Token dccec1ad173d2abaf88b542a02095f8d93ea97df",
+  # "Token 8271c9035b3a113a16111392722a7bb4d9278a2c",
+  # "Token 64936db353e36faa7ec880bb81331706cd4216a7"
+timer = {'time': 0, 'purpose': 'item lookup'}
+room_info = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=header_info)
+item_to_get = ''
 
 # this is the regular path
 def traversal(player, graph):
@@ -42,72 +27,72 @@ def traversal(player, graph):
   #   visited_rooms.add(room_info[rooms]['room_id'])
   while rooms_to_visit:
 
-    for room in room_info:
-      purpose = timers[room]['purpose']
-      time = timers[room]['time']
-      user = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/', headers={'Authorization': api_keys[room]})
-      ## TODO post user status into our version of user
-      ## TODO post room data into our version of the room 
-      time = user['cooldown']
+    purpose = timer['purpose']
+    time = timer['time']
+    user = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/', headers=header_info)
+    ## TODO post user status into our version of user
+    ## TODO post room data into our version of the room 
+    time = user['cooldown']
 
-      ## for each user, check
-      ## - purpose of timer - tells us which actions we are performing
-      ## - see if timer time has reached zero
+    ## for each user, check
+    ## - purpose of timer - tells us which actions we are performing
+    ## - see if timer time has reached zero
 
-      ## ITEM LOOKUP
-      while purpose == "item lookup":
-        if len(room_info[room]['items']) == 0:
-          purpose = 'move'
+    ## ITEM LOOKUP
+    while purpose == "item lookup":
+      if len(room_info['items']) == 0:
+        purpose = 'move'
       ## check the timer
-        if time <= 0:
-        ## TODO get the info from our database for the room we are in
-          for item in room_info[room]['items']:
-            pickup = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/', headers={'Authorization': api_keys[room]}, json={"name":item['name']})
-            time = pickup['cooldown']
-            if pickup['weight'] + user['encumbrance'] <= user['strength']:
-              item_to_get[room] = [pickup['name']]
-              purpose = 'item get'
-            else:
-              purpose = 'find shop'
-            
-      ## ITEM GET
-      while purpose == 'item get':
-        if time <= 0:
-          received_item = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers={'Authorization': api_key[room]}, json={"name":item_to_get[room]})
-          time = received_item['cooldown']
-          purpose = 'item lookup'
+      if time <= 0:
+      ## TODO get the info from our database for the room we are in
+        for item in room_info['items']:
+          item_name = {"name": item['name']}
+          pickup = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/', headers=header_info, json=item_name)
+          time = pickup['cooldown']
+          if pickup['weight'] + user['encumbrance'] <= user['strength']:
+            item_to_get = pickup['name']
+            purpose = 'item get'
+          else:
+            purpose = 'find shop'
+          
+    ## ITEM GET
+    while purpose == 'item get':
+      if time <= 0:
+        received_item = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers={'Authorization': api_key[room]}, json={"name":item_to_get})
+        time = received_item['cooldown']
+        purpose = 'item lookup'
 
-      ## DIRECTION DECISION TREE
-      ## check timer again
-      c_rm = room_info[room]
-      c_rm_n = True if 'n' in room_info[room]['exits'] and blahblahblah['n'] is not Null else False
-      c_rm_s = True if 's' in room_info[room]['exits'] else False
-      c_rm_e = True if 'e' in room_info[room]['exits'] else False
-      c_rm_w = True if 'w' in room_info[room]['exits'] else False
+    ## DIRECTION DECISION TREE
+    ## check timer again
+    c_rm = room_info[room]
+    c_rm_n = True if 'n' in room_info[room]['exits'] and blahblahblah['n'] is not Null else False
+    c_rm_s = True if 's' in room_info[room]['exits'] else False
+    c_rm_e = True if 'e' in room_info[room]['exits'] else False
+    c_rm_w = True if 'w' in room_info[room]['exits'] else False
 
-      if c_rm_n and c_rm['room_id'] not in visited_rooms:
-        visited_rooms.add(c_rm['room_id'])
-        new_room = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"n"})
-        timers[room] = new_room["cooldown"]
-        ## add post command to add the new room and add the "s" of the new room in our database to the old room id
-        ## add put command to change the "n" of the old room in our database
-        room_info[room] = dict(new_room)
-      elif c_rm_e and c_rm['room_id'] not in visited_rooms:
-        visited_rooms.add(c_rm['room_id'])
-        requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"e"})
-      elif c_rm_s and c_rm['room_id'] not in visited_rooms:
-        visited_rooms.add(c_rm['room_id'])
-        requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"s"})
-      elif c_rm_w and c_rm['room_id'] not in visited_rooms:
-        visited_rooms.add(c_rm['room_id'])
-        requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"w"})
-      elif len(roomGraph.keys()) == len(visited_rooms):
-        rooms_to_visit = False
-      else:
-        last_open = nearest_open_path(c_rm.id, graph, visited_rooms)
-        for d in last_open['path']:
-          traversalPath.append(d)
-          player.travel(d)
+    if c_rm_n and c_rm['room_id'] not in visited_rooms:
+      visited_rooms.add(c_rm['room_id'])
+      new_room = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"n"})
+      timers[room] = new_room["cooldown"]
+      ## add post command to add the new room and add the "s" of the new room in our database to the old room id
+      ## add put command to change the "n" of the old room in our database
+      room_info[room] = dict(new_room)
+    elif c_rm_e and c_rm['room_id'] not in visited_rooms:
+      visited_rooms.add(c_rm['room_id'])
+      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"e"})
+    elif c_rm_s and c_rm['room_id'] not in visited_rooms:
+      visited_rooms.add(c_rm['room_id'])
+      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"s"})
+    elif c_rm_w and c_rm['room_id'] not in visited_rooms:
+      visited_rooms.add(c_rm['room_id'])
+      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"w"})
+    elif len(roomGraph.keys()) == len(visited_rooms):
+      rooms_to_visit = False
+    else:
+      last_open = nearest_open_path(c_rm.id, graph, visited_rooms)
+      for d in last_open['path']:
+        traversalPath.append(d)
+        player.travel(d)
 
 
 
