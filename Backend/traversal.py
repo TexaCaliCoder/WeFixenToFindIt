f4b31956ef5f61432ef70869631d7aae023a0bc7
Michael Littleton <michaelglittleton@gmail.com>
2019-09-05 23:03:28 -0400

    Cleaned up a bit and added some functionality
    Added random walk and timer functionality
    
    Co-authored-by: Dewayne Lewis Lindsay Jr. <dewaynelindsay87@gmail.com>

--- Backend/traversal.py
+++ Backend/traversal.py
@@ -1,7 +1,6 @@
 import requests
 import random
-
-# send init command
+from datetime import datetime, timedelta
 
 traversalPath = []
 '''
@@ -13,8 +12,9 @@ header_info = {'Authorization': api_key}
   # "Token dccec1ad173d2abaf88b542a02095f8d93ea97df",
   # "Token 8271c9035b3a113a16111392722a7bb4d9278a2c",
   # "Token 64936db353e36faa7ec880bb81331706cd4216a7"
-timer = {'time': 0, 'purpose': 'item lookup'}
+timer = {'time': 0, 'purpose': 'move purposefully'}
 room_info = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=header_info)
+timer['time'] = countdown_setup(room_info['cooldown'])
 item_to_get = ''
 
 # this is the regular path
@@ -27,74 +27,123 @@ def traversal(player, graph):
   #   visited_rooms.add(room_info[rooms]['room_id'])
   while rooms_to_visit:
 
+    global room_info
     purpose = timer['purpose']
-    time = timer['time']
     user = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/', headers=header_info)
+    timer['time'] = countdown_setup(user['cooldown'])
     ## TODO post user status into our version of user
-    ## TODO post room data into our version of the room 
-    time = user['cooldown']
+    ## TODO post room data into our version of the room
 
     ## for each user, check
     ## - purpose of timer - tells us which actions we are performing
     ## - see if timer time has reached zero
 
     ## ITEM LOOKUP
+
     while purpose == "item lookup":
       if len(room_info['items']) == 0:
-        purpose = 'move'
+        purpose = 'move randomly'
       ## check the timer
-      if time <= 0:
+      if timecheck():
       ## TODO get the info from our database for the room we are in
         for item in room_info['items']:
           item_name = {"name": item['name']}
           pickup = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/', headers=header_info, json=item_name)
-          time = pickup['cooldown']
+          timer['time'] = countdown_setup(pickup['cooldown'])
           if pickup['weight'] + user['encumbrance'] <= user['strength']:
             item_to_get = pickup['name']
             purpose = 'item get'
           else:
             purpose = 'find shop'
-          
+
+    while purpose == "move randomly":
+      possible_moves = []
+      c_rm = room_info
+        ## TODO add in exits info from database version of the room
+      c_rm_n = True if 'n' in room_info['exits'] else False
+      c_rm_s = True if 's' in room_info['exits'] else False
+      c_rm_e = True if 'e' in room_info['exits'] else False
+      c_rm_w = True if 'w' in room_info['exits'] else False
+      if c_rm_n:
+        possible_moves.append("n")
+      if c_rm_s:
+        possible_moves.append("s")
+      if c_rm_e:
+        possible_moves.append("e")
+      if c_rm_w:
+        possible_moves.append("w")
+      move = random.choice(possible_moves)
+
+      if timecheck():
+          new_room = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=header_info, json={"direction":move})
+          timer['time'] = countdown_setup(new_room['cooldown'])
+          ## TODO add post command to add the new room and add the "w" of the new room in our database to the old room id
+          ## TODO add put command to change the "e" of the old room in our database
+          purpose == "item lookup"
+
     ## ITEM GET
     while purpose == 'item get':
-      if time <= 0:
+      if timecheck():
         received_item = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers=header_info, json={"name":item_to_get})
-        time = received_item['cooldown']
+        timer['time'] = countdown_setup(received_item['cooldown'])
         purpose = 'item lookup'
 
     ## DIRECTION DECISION TREE
     ## check timer again
-    c_rm = room_info[room]
-    c_rm_n = True if 'n' in room_info[room]['exits'] and blahblahblah['n'] is not Null else False
-    c_rm_s = True if 's' in room_info[room]['exits'] else False
-    c_rm_e = True if 'e' in room_info[room]['exits'] else False
-    c_rm_w = True if 'w' in room_info[room]['exits'] else False
+    while purpose == 'move purposefully':
+      if timecheck():
+        c_rm = room_info
+        ## TODO add in exits info from database version of the room
+        c_rm_n = True if 'n' in room_info['exits'] else False
+        c_rm_s = True if 's' in room_info['exits'] else False
+        c_rm_e = True if 'e' in room_info['exits'] else False
+        c_rm_w = True if 'w' in room_info['exits'] else False
 
-    if c_rm_n and c_rm['room_id'] not in visited_rooms:
-      visited_rooms.add(c_rm['room_id'])
-      new_room = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"n"})
-      timers[room] = new_room["cooldown"]
-      ## add post command to add the new room and add the "s" of the new room in our database to the old room id
-      ## add put command to change the "n" of the old room in our database
-      room_info[room] = dict(new_room)
-    elif c_rm_e and c_rm['room_id'] not in visited_rooms:
-      visited_rooms.add(c_rm['room_id'])
-      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"e"})
-    elif c_rm_s and c_rm['room_id'] not in visited_rooms:
-      visited_rooms.add(c_rm['room_id'])
-      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"s"})
-    elif c_rm_w and c_rm['room_id'] not in visited_rooms:
-      visited_rooms.add(c_rm['room_id'])
-      requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers={'Authorization': api_keys[room]}, json={"direction":"w"})
-    elif len(roomGraph.keys()) == len(visited_rooms):
-      rooms_to_visit = False
-    else:
-      last_open = nearest_open_path(c_rm.id, graph, visited_rooms)
-      for d in last_open['path']:
-        traversalPath.append(d)
-        player.travel(d)
+        if c_rm_n and c_rm['room_id'] not in visited_rooms:
+          visited_rooms.add(c_rm['room_id'])
+          new_room = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=header_info, json={"direction":"n"})
+          timer['time'] = countdown_setup(new_room["cooldown"])
+          ## TODO add post command to add the new room and add the "s" of the new room in our database to the old room id
+          ## TODO add put command to change the "n" of the old room in our database
+          room_info = dict(new_room)
+        elif c_rm_e and c_rm['room_id'] not in visited_rooms:
+          visited_rooms.add(c_rm['room_id'])
+          requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=header_info, json={"direction":"e"})
+          timer['time'] = countdown_setup(new_room["cooldown"])
+          ## TODO add post command to add the new room and add the "w" of the new room in our database to the old room id
+          ## TODO add put command to change the "e" of the old room in our database
+          room_info = dict(new_room)
+        elif c_rm_s and c_rm['room_id'] not in visited_rooms:
+          visited_rooms.add(c_rm['room_id'])
+          requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=header_info, json={"direction":"s"})
+          timer['time'] = countdown_setup(new_room["cooldown"])
+          ## TODO add post command to add the new room and add the "n" of the new room in our database to the old room id
+          ## TODO add put command to change the "s" of the old room in our database
+          room_info = dict(new_room)
+        elif c_rm_w and c_rm['room_id'] not in visited_rooms:
+          visited_rooms.add(c_rm['room_id'])
+          requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=header_info, json={"direction":"w"})
+          timer['time'] = countdown_setup(new_room["cooldown"])
+          ## TODO add post command to add the new room and add the "e" of the new room in our database to the old room id
+          ## TODO add put command to change the "w" of the old room in our database
+          room_info = dict(new_room)
+        elif len(visited_rooms) == 500:
+          purpose = 'move randomly'
+        else:
+          last_open = nearest_open_path(c_rm.id, graph, visited_rooms)
+          for d in last_open['path']:
+            traversalPath.append(d)
+            player.travel(d)
 
 
+def countdown_setup(cooldown):
+  return datetime.now() + timedelta(seconds=cooldown)
+
+def timecheck():
+  if timer['time'] < datetime.now():
+    return True
+  else:
+    return False
 
 # This is the breadth first search for a room that still has openings
 def nearest_open_path(start, graph, visited):
@@ -117,22 +166,6 @@ def nearest_open_path(start, graph, visited):
 
 # go to shop and sell items
 
-traversal(player, roomGraph)
-
-# TRAVERSAL TEST
-visited_rooms = set()
-player.currentRoom = world.startingRoom
-visited_rooms.add(player.currentRoom)
-for move in traversalPath:
-    player.travel(move)
-    visited_rooms.add(player.currentRoom)
-
-if len(visited_rooms) == len(roomGraph):
-    print(f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
-
 
 class Queue():
     def __init__(self):
@@ -146,13 +179,4 @@ class Queue():
             return None
     def size(self):
         return len(self.queue)
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-# player.currentRoom.printRoomDescription(player)
-# while True:
-#     cmds = input("-> ").lower().split(" ")
-#     if cmds[0] in ["n", "s", "e", "w"]:
-#         player.travel(cmds[0], True)
-#     else:
-#         print("I did not understand that command.")
+
