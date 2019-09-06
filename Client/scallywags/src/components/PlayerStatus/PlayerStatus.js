f0789f25023ff33218837813a653fb6153b16418
Michael Trew <mtrew2015@gmail.com>
2019-09-05 23:09:14 -0400

    added some stuff to room info and player info

--- Client/scallywags/src/components/PlayerStatus/PlayerStatus.js
+++ Client/scallywags/src/components/PlayerStatus/PlayerStatus.js
@@ -5,9 +5,11 @@ export default class PlayerStatus extends Component {
     render() {
         return (
             <div className="sideBlock">
-                <p>Name</p>
-                <p>Encumbrance</p>
-                <p>Speed</p>
+                <p>Name:</p>
+                <p>Gold:</p>
+                <p>Encumbrance:</p>
+                <p>Speed:</p>
+                <p>Inventory:</p>
             </div>
         )
     }
--- Client/scallywags/src/components/RoomInfo /RoomInfo.js
+++ Client/scallywags/src/components/RoomInfo /RoomInfo.js
@@ -5,6 +5,7 @@ export default class RoomInfo extends Component {
         return (
             <div className="sideBlock">
                 <p>Room Name:</p>
+                <p>Room Coordinates</p>
                 <p>Items In Room:</p>
                 <p>Players In Room:</p>
             </div>
