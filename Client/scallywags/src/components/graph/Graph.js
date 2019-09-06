d84053155e5e9bb1a780abadfed2789bffb16469
Michael Trew <mtrew2015@gmail.com>
2019-09-05 22:38:36 -0400

    now its a plot graph

--- Client/scallywags/src/components/graph/Graph.js
+++ Client/scallywags/src/components/graph/Graph.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './Graph.scss';
-import { FlexibleXYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines } from 'react-vis';
+import { FlexibleXYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines, MarkSeries } from 'react-vis';
 
 export default class graph extends Component {
     render() {
@@ -12,15 +12,16 @@ export default class graph extends Component {
                     height={600}>
                     <HorizontalGridLines />
                     <VerticalGridLines/>
-                    <LineSeries
-                    strokeWidth = "2"
-                    color="crimson"
+                    <MarkSeries
+                    strokeWidth={3}
+                    opacity = "1"
                         data={[
                             { x: 1, y: 1 },
                             { x: 2, y: 2 },
                             { x: 3, y:3 },
                             { x: 4, y: 4},
-                            { x: -5, y: -5}
+                            { x: -5, y: -5},
+                            { x: 2, y: 10}
                         ]} />
                     <XAxis />
                     <YAxis />
