89de5f990b126a90e9890d95032ff5c57147999d
Michael Trew <mtrew2015@gmail.com>
2019-09-05 21:05:44 -0400

    added some file structure

---
+++ Client/scallywags/src/views/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Home.scss';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
