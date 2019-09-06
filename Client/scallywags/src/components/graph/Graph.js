2ae042f56cceb35c62d5c8d9b71dc7e787273f8e
Michael Trew <mtrew2015@gmail.com>
2019-09-05 22:33:23 -0400

    working on the chart...

--- Client/scallywags/src/components/graph/Graph.js
+++ Client/scallywags/src/components/graph/Graph.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import './Graph.scss';
+import { FlexibleXYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines } from 'react-vis';
 
 export default class graph extends Component {
     render() {
         return (
             <div className="graph">
                 <h2>Graph Will Go Here</h2>
+                <FlexibleXYPlot
+                    width={600}
+                    height={600}>
+                    <HorizontalGridLines />
+                    <VerticalGridLines/>
+                    <LineSeries
+                    strokeWidth = "2"
+                    color="crimson"
+                        data={[
+                            { x: 1, y: 1 },
+                            { x: 2, y: 2 },
+                            { x: 3, y:3 },
+                            { x: 4, y: 4},
+                            { x: -5, y: -5}
+                        ]} />
+                    <XAxis />
+                    <YAxis />
+                </FlexibleXYPlot>
             </div>
         )
     }
