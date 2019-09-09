696c6e1dbe93aa7e026df2892b92563b32ef6aed
Chris Roth <c.roth.lambda@gmail.com>
2019-09-08 21:39:00 -0500

    deleted unused files

--- Backend/traversal.py
+++ Backend/traversal.py
@@ -20,7 +20,7 @@ def get_room_dict():
 
   return room_dict
 
-api_key = "Token 508711f53445fa67d8bdc1c97da256eacaef2e5e"
+api_key = "Token 64936db353e36faa7ec880bb81331706cd4216a7"
 header_info = {'Authorization': api_key}
   # "Token dccec1ad173d2abaf88b542a02095f8d93ea97df",
   # "Token 8271c9035b3a113a16111392722a7bb4d9278a2c",
