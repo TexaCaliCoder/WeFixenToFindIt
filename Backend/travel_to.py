c8830851a8c3d2bf85c77fe3bed7ddced93ad0d4
Michael Littleton <michaelglittleton@gmail.com>
2019-09-09 18:10:57 -0400

    Gathering treasure and selling initial attempt

---
+++ Backend/travel_to.py
@@ -0,0 +1,16 @@
+import requests
+from traversal import Queue, get_room_dict, shortest_path_to, follow_path
+
+start = 9
+
+shop = 1
+pirate_ry = 467
+flying_shrine = 22
+ghost_shrine = 499
+speed_shrine = 461
+
+path = shortest_path_to(0, start)
+
+follow_path(start, path['path'])
+
+
