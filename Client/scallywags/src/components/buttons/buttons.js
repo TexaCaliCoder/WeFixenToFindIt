27bab9adb0275689e22965bcac31b9f7a0097025
Michael Trew <mtrew2015@gmail.com>
2019-09-09 21:13:33 -0400

    fixed some alignment

--- Client/scallywags/src/components/buttons/buttons.js
+++ Client/scallywags/src/components/buttons/buttons.js
@@ -5,7 +5,7 @@ import { button } from 'react-router-dom';
 class Buttons extends Component {
 	render() {
 		return (
-			<div>
+			<div className="buttonBar">
 				<button className="directionButton">North</button>
                 <button className="directionButton">South</button>
                 <button className="directionButton">East</button>
